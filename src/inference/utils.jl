cc75a4b6bcfd039d9e90f3db8971d4b0930a5a18
Luca Bittarello <luca.bittarello@gmail.com>
2017-11-26 15:47:14 -0600

    Rearranged folders

---
+++ src/inference/utils.jl
@@ -0,0 +1,30 @@
+#==========================================================================================#
+
+# DISTANCE ON THE GLOBE
+
+havd(x::Float64) = 0.5 * (1.0 - cosd(x))
+
+function geodistance(y0::Float64, x0::Float64, y1::Float64, x1::Float64)
+    h = havd(y1 - y0) + havd(x1 - x0) * cosd(y0) * cosd(y1)
+    return 12742.0 * asin(min(1.0, sqrt(h)))
+end
+
+#==========================================================================================#
+
+# KERNELS
+
+bartlett(x::Float64)     = (abs(x) >= 1.0) ? 0.0 : (1.0 - abs(x))
+truncated(x::Float64)    = (abs(x) >= 1.0) ? 0.0 : 1.0
+tukeyhanning(x::Float64) = (abs(x) >= 1.0) ? 0.0 : 0.5 * (1.0 + cospi(x))
+
+function parzen(x::Float64)
+    if abs(x) <= 0.5
+        return 1.0 - 6.0 * abs2(x) + 6 * abs(x)^3
+    elseif abs(x) <= 1.0
+        return 2.0 * (1.0 - abs(x))^3
+    else
+        return 0.0
+    end
+end
+
+gallant(x::Float64) = parzen(x)
