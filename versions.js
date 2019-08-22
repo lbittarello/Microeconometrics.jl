ae3aac636141f90923404d46525dd59e8276e961
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-22 09:09:33 +0000

    build based on 403cfe2

--- versions.js
+++ versions.js
@@ -1,12 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-0.4",
-  "release-0.3",
-  "release-0.2",
-  "v0.4.2",
-  "v0.4.1",
-  "v0.4.0",
-  "v0.3.0",
-  "v0.2.0",
+  "v0.4",
+  "v0.3",
+  "v0.2",
+  "dev",
 ];
